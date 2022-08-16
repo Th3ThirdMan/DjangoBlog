5b3199210b2f8d4bd14c58fe134455bd238eee11
Th3ThirdMan <davidgkennedymail@gmail.com>
2022-08-16 19:41:19 +0000

    Update Deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['mb2test.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestarapp2022.herokuapp.com', 'localhost']
 
 # Application definition
 
